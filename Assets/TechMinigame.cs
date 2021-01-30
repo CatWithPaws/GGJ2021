fdbfd5c6c4ba089a35d215f323f80441d6ba6775
IdlyBond <53187706+IdlyBond@users.noreply.github.com>
2021-01-31 00:23:18 +0200

    Fork Refactor

--- Assets/TechMinigame.cs
+++ Assets/TechMinigame.cs
@@ -6,6 +6,6 @@ public class TechMinigame : MonoBehaviour
     public GameObject platformPrefab;
     void Start()
     {
-        
+        WorldBroadcast.CollectionGameEnded.Publish(gameObject);
     }
 }
