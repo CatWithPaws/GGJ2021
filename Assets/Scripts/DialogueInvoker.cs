97c17a9987e3455e1f6cc58101d70d15378ecd8e
IdlyBond <53187706+IdlyBond@users.noreply.github.com>
2021-01-30 13:47:08 +0200

    Dialogue system

--- Assets/Scripts/DialogueInvoker.cs
+++ Assets/Scripts/DialogueInvoker.cs
@@ -1,18 +1,39 @@
+using System;
 using UnityEngine;
 
 public class DialogueInvoker : MonoBehaviour
 {
     [SerializeField] private Dialogue[] dialogues;
 
-    private void OnTriggerStay2D(Collider2D other)
+    private bool isPlayer;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Players") && Input.GetKeyDown(KeyCode.E))
+        if (other.CompareTag("Players"))
+        {
+            isPlayer = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Players"))
+        {
+            isPlayer = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayer && Input.GetKeyDown(KeyCode.E))
         {
             foreach (var d in dialogues)
             {
                 DialogueManager.AddDialogue(d);
             }
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            isPlayer = false;
         }
     }
 }
--- Assets/Scripts/LoadLevelOnTrigger.cs
+++ Assets/Scripts/LoadLevelOnTrigger.cs
@@ -6,10 +6,7 @@ public class LoadLevelOnTrigger : MonoBehaviour
 {
 	
 	[SerializeField] private string sceneToLoad;
-
-	private void Awake()
-	{
-	}
+	
 	private void OnCollisionEnter2D(Collision2D other)
 	{
 		print(other.collider.name);
@@ -19,13 +16,4 @@ public class LoadLevelOnTrigger : MonoBehaviour
 			StartCoroutine(GlobalVars.i.FadeIn(sceneToLoad));
 		}
 	}
-	//private void OnTriggerEnter2D(Collider2D other)
-	//{
-	//	print(other.gameObject.name);
-	//	if (other.gameObject && other.gameObject.CompareTag("Players"))
-	//	{
-	//		print("Collided with levelloader");
-	//		StartCoroutine(GlobalVars.i.FadeIn(sceneToLoad));
-	//	}
-	//}
 }
