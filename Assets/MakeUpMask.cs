cc7f12f474b2917ee17db0e5ad96b0b97dcea1b2
ValenInWork <valentin.kulakov@qplaze.com>
2021-01-30 16:26:12 +0200

    Make MiniGames

---
+++ Assets/MakeUpMask.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MakeUpMask : MonoBehaviour
+{
+
+	[SerializeField] ImageType[] PicturesToCompare;
+
+	[SerializeField] bool[] campares = new bool[4];
+
+	[SerializeField] GameObject MiniGameCanvas;
+
+	public void MovePartPicture(RectTransform rectTransform)
+	{
+		rectTransform.position = Input.mousePosition;
+	}
+
+	public void ComparePicturesParts(ImageType movableImage)
+	{
+		foreach(ImageType imageItem in PicturesToCompare)
+		{
+			float xPos = Mathf.Abs(imageItem.gameObject.GetComponent<RectTransform>().position.x - movableImage.gameObject.GetComponent<RectTransform>().position.x);
+			float yPos = Mathf.Abs(imageItem.gameObject.GetComponent<RectTransform>().position.y - movableImage.gameObject.GetComponent<RectTransform>().position.y);
+			print(xPos);
+			print(yPos);
+			if (imageItem.ID == movableImage.ID)
+			{
+				print("compare");
+				if (xPos < 100 && yPos < 100)
+				{
+					campares[imageItem.ID] = true;
+					movableImage.gameObject.gameObject.GetComponent<RectTransform>().position = imageItem.gameObject.gameObject.GetComponent<RectTransform>().position;
+					if(campares[0] && campares[1] && campares[2] && campares[3])
+					{
+						GamePassed();
+					}
+				}
+			}
+			else print("dont");
+			
+		}
+	}
+
+	void GamePassed()
+	{
+		MiniGameCanvas.SetActive(false);
+	}
+
+}
